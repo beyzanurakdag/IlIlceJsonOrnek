0903a1b05a227f6a9da01b2b9efab77663c62440
103SABAH_BEYZANUR <103SABAH_BEYZANUR@DESKTOP-TUMHS1A>
2021-12-02 13:30:30 +0300

    Add project files.

---
+++ IlIlceJson_EntityModels/Classlar/IL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlIlceJson_EntityModels.Classlar
+{
+    public class IL
+    {
+        public string ILAdi { get; set; }
+        public string Plaka { get; set; }
+        public List<string> ILceleri { get; set; }
+
+    }
+}
